cad2e3d8993e763bea71613eb1a19d8b7aaa3a32
supra-jatinjainsaraf <j.saraf@supraoracles.com>
2024-01-25 13:56:27 +0530

    a

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,5 @@
 import app from './routes';
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
