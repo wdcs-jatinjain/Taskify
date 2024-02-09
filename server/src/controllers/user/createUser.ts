7d8fc8c959f98051ef7815a066188c6ed8c05f80
supra-tarunpatel <t.patel@supraoracles.com>
2024-02-09 16:47:18 +0530

    fixed issues.

--- server/src/controllers/user/createUser.ts
+++ server/src/controllers/user/createUser.ts
@@ -1,9 +1,10 @@
+import { Response } from "express";
 import Views from "../../views";
 import { createUserValidation } from './userValidations';
 import { registerbody } from "../../types";
 
 
-export default async function createUserController({ body: { name, email, password, contactNo } }: { body: registerbody }, res: any) {
+export default async function createUserController({ body: { name, email, password, contactNo } }: { body: registerbody }, res: Response) {
 
     try {
         await createUserValidation.validateAsync({ name, email, password, contactNo }, { abortEarly: false });
