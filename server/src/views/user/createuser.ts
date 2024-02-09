0ec90fde9da56182c5dfc38fed9573397e2a7726
supra-tarunpatel <t.patel@supraoracles.com>
2024-02-09 17:35:34 +0530

    added token generation.

--- server/src/views/user/createuser.ts
+++ server/src/views/user/createuser.ts
@@ -1,6 +1,9 @@
 import UserModel from '../../models/user/index';
 import { registerbody } from '../../types';
-import { RESULT_STATUS } from '../../constants'
+import { RESULT_STATUS } from '../../constants';
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = 'your_secret_key';
 
 export default async function createUser({ name, email, password, contactNo }: registerbody) {
 
@@ -15,9 +18,15 @@ export default async function createUser({ name, email, password, contactNo }: r
         }
 
         const newUser = await UserModel.create({ name, email, password, contactNo });
+        const newToken = jwt.sign({ userId: newUser._id }, JWT_SECRET, { expiresIn: '1h' });
+
         return {
             status: RESULT_STATUS.SUCCESS,
-            message: "User Created successfully"
+            message: "User Created successfully",
+            data: {
+                userId: newUser._id ,
+                token: newToken
+            }
         }
     } catch (error) {
         console.error("🚀 ~ createUser ~ error:", error)
